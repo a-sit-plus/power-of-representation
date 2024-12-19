6c0ca1bd52287f57946a86d20b378393d3a3b439
Bernd Prünster <bernd.pruenster@a-sit.at>
2024-12-19 21:52:19 +0100

    Release 1.1.1

--- powerofrepresentation/build.gradle.kts
+++ powerofrepresentation/build.gradle.kts
@@ -22,7 +22,7 @@ kotlin {
     sourceSets {
         commonMain {
             dependencies {
-                api("at.asitplus.wallet:vck:5.0.0")
+                api("at.asitplus.wallet:vck:5.2.1")
             }
         }
     }
